2b40c04a8eac242939b0b0a42e96eb264ffb0552
Jordan Payne <payne.jc@me.com>
2016-03-21 15:56:44 -0300

    add turnover differential, offensive strategy and pyth wins.

--- nfl_predict.py
+++ nfl_predict.py
@@ -15,26 +15,24 @@ with nfldb.Tx(db) as cursor:
         teams.append(row)
 
 weekly_data = {}
-for week in range(1,5):
+for week in range(1,18):
     weeks_data = {}
     # for all 17 weeks, calculate the winning percentage of each team t so far in the season (since week i+1)
     for t in teams:
         team = t['team_id']
         q = nfldb.Query(db)
+        this_team_played = nfldb.QueryOR(db).game(home_team=team).game(away_team=team)
         q.game(season_year=2009, season_type='Regular', week__lt=week)
-        games_played = q.as_games()
-        wins = len([g for g in games_played if g.winner == team])
-        loses = len([g for g in games_played if g.loser == team])
+        all_previous_games = q.andalso(this_team_played).as_games()
+        wins = len([g for g in all_previous_games if g.winner == team])
+        loses = len([g for g in all_previous_games if g.loser == team])
         try:
             WP = (wins/float((wins+loses)))
         except ZeroDivisionError:
             WP = 0.0
         weeks_data[team] = {'wp': WP}
 
-        q = nfldb.Query(db)
-        this_team_played = nfldb.QueryOR(db).game(home_team=team).game(away_team=team)
-        q.game(season_year=2009, season_type='Regular', week__lt=week)
-        all_previous_games = q.andalso(this_team_played).as_games()
+
         opponents = [g.away_team if g.home_team == team else g.home_team for g in all_previous_games]
         opponents_records = []
         for o in opponents:
@@ -65,7 +63,55 @@ for week in range(1,5):
             OOWP = 0.0
         weeks_data[team]['oowp'] = OOWP
         weeks_data[team]['rpi'] = 0.25 * WP + 0.5 + OWP * 0.5 + 0.25 * OOWP
+
+        points_for = sum([g.home_score if g.home_team == team else g.away_score for g in all_previous_games])
+        points_against = sum([g.away_score if g.home_team == team else g.home_score for g in all_previous_games])
+        try:
+            pyth_wins = pow(points_for, 2.37)/(pow(points_for, 2.37) + pow(points_against, 2.37))
+        except ZeroDivisionError:
+            pyth_wins = 0
+        weeks_data[team]['pyth_wins'] = pyth_wins
+
+        q = nfldb.Query(db)
+        q.game(season_year=2015, season_type='Regular', week__lt=week, team=team)
+        passing_attempts = 0
+        rushing_attempts = 0
+        for pp in q.drive(pos_team=team).as_aggregate():
+            passing_attempts = passing_attempts + pp.passing_att
+            rushing_attempts = rushing_attempts + pp.rushing_att
+        try:
+            offensive_strategy = passing_attempts/float(rushing_attempts)
+        except ZeroDivisionError:
+            offensive_strategy = 0.0
+        weeks_data[team]['offensive_strategy'] = offensive_strategy
+
+        q = nfldb.Query(db)
+        q.game(season_year=2009, season_type='Regular', week__lt=week, team=team)
+        interceptions = 0
+        fumbles_recovered = 0
+        for pp in q.drive(pos_team__ne=team).as_aggregate():
+            interceptions = interceptions + pp.defense_int
+            fumbles_recovered = fumbles_recovered + pp.defense_frec
+
+        q = nfldb.Query(db)
+        q.game(season_year=2009, season_type='Regular', week__lt=week, team=team)
+        interceptions_lost = 0
+        fumbles_lost = 0
+        for pp in q.drive(pos_team=team).as_aggregate():
+            interceptions_lost = interceptions_lost + pp.passing_int
+            fumbles_lost = fumbles_lost + pp.fumbles_lost
+
+        takeaways = interceptions + fumbles_recovered
+        giveaways = interceptions_lost + fumbles_lost
+
+        try:
+            turnover_differential = takeaways/float(giveaways)
+        except ZeroDivisionError:
+            turnover_differential = 0.0
+        weeks_data[team]['turnover_differential'] = turnover_differential
+
     weekly_data[str(week)] = weeks_data
+
 
 for k, v in weekly_data.iteritems():
     print '\nWEEK %s' % k
