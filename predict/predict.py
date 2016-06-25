56b4364f424ef9577b5c0ede71dd11a0fe4663f4
Jordan Payne <payne.jc@me.com>
2016-06-25 19:21:33 -0300

    Finish basic winner predictor, 72.6% accuracy for 2015 season

--- predict/predict.py
+++ predict/predict.py
@@ -2,6 +2,25 @@
 
 import json
 import generate
+import math
+import nfldb
+
+db = nfldb.connect()
+
+def normalize(data, game_profile):
+    for s in range(1, len(data[0]['home_stats'])):
+        stat = [d['home_stats'][s] for d in data]
+        stat.extend([d['away_stats'][s] for d in data])
+        stat.append(game_profile['home_stats'][s])
+        stat.append(game_profile['away_stats'][s])
+        stat_max = max(stat)
+        stat_min = min(stat)
+        for d in data:
+            d['home_stats'][s] = (d['home_stats'][s] - stat_min)/(stat_max - float(stat_min))
+            d['away_stats'][s] = (d['away_stats'][s] - stat_min)/(stat_max - float(stat_min))
+        game_profile['home_stats'][s] = (game_profile['home_stats'][s] - stat_min) / (stat_max - float(stat_min))
+        game_profile['away_stats'][s] = (game_profile['away_stats'][s] - stat_min) / (stat_max - float(stat_min))
+    return data, game_profile
 
 def open_data():
     f = open('data.json', 'r')
@@ -12,6 +31,7 @@ def open_data():
 def compare_teams(game):
     game_profile = generate.gen_profile(game)
     data = open_data()
+    data, game_profile = normalize(data, game_profile)
     limit = 6
     closest = []
     for g in data:
@@ -41,9 +61,116 @@ def calculate_distance(game_stats1, game_stats2):
     return round(distance, 5)
 
 def derive_scores(profile):
+    try:
+        scores = [
+        round(math.log10(profile[profile['distance_config'][0]])/profile['distance'], 5),
+        round(math.log10(profile[profile['distance_config'][1]])/profile['distance'], 5)]
+    except ValueError:
+        scores = [0,0]
+        if profile[profile['distance_config'][0]] == 0:
+            scores[0] = 0
+            scores[1] = round(math.log10(profile[profile['distance_config'][1]])/profile['distance'], 5)
+        else:
+            scores[1] = 0
+            scores[0] = round(math.log10(profile[profile['distance_config'][0]])/profile['distance'], 5)
+    return scores
 
-    print profile[profile['distance_config'][0]], profile[profile['distance_config'][1]], profile['distance']
+def predict_winner(profiles, game):
+    scores = [0,0]
+    for p in profiles:
+        score = derive_scores(p)
+        scores[0] += score[0]
+        scores[1] += score[1]
 
-    return [
-    round(profile[profile['distance_config'][0]]/profile['distance'], 5),
-    round(profile[profile['distance_config'][1]]/profile['distance'], 5)]
+    # scores[0] *= home_field_multiplier[game.home_team]
+    # scores[1] *= 1 - home_field_multiplier[game.home_team]
+    #
+    # scores[0] *= 1 - away_field_multiplier[game.away_team]
+    # scores[1] *= away_field_multiplier[game.away_team]
+
+    winner = game.home_team if scores[0] > scores[1] else game.away_team
+    return winner
+
+def predict_winners(year, season_type):
+    q = nfldb.Query(db)
+    q.game(season_year=year, season_type=season_type)
+    games = q.as_games()
+    counter = 0
+    print len(games)
+    for g in games:
+        results = compare_teams(g)
+        winner = predict_winner(results, g)
+        result = 'Correct' if winner == g.winner else 'Incorrect'
+        if winner == g.winner:
+            counter += 1
+    return counter
+
+home_field_multiplier = {
+    'CAR': 0.5250,
+    'IND': 0.7375,
+    'GB': 0.6875,
+    'TEN': 0.5875,
+    'NE': 0.8375,
+    'NYG': 0.5250,
+    'JAC': 0.5750,
+    'TB': 0.5250,
+    'ATL': 0.6250,
+    'NO': 0.5625,
+    'SEA': 0.6375,
+    'BAL': 0.7625,
+    'DET': 0.4000,
+    'DEN': 0.6000,
+    'ARI': 0.5500,
+    'MIN': 0.6000,
+    'BUF': 0.4875,
+    'MIA': 0.4750,
+    'CLE': 0.3875,
+    'CHI': 0.6125,
+    'STL': 0.4375,
+    'OAK': 0.3875,
+    'SD': 0.6875,
+    'PIT': 0.7342,
+    'WAS': 0.4500,
+    'PHI': 0.6250,
+    'NYJ': 0.5750,
+    'CIN': 0.5190,
+    'HOU': 0.4875,
+    'KC': 0.5625,
+    'SF': 0.5500,
+    'DAL': 0.6000
+}
+
+away_field_multiplier = {
+    'PHI': 0.6203,
+    'IND': 0.6500,
+    'CAR': 0.4625,
+    'GB': 0.5500,
+    'TEN': 0.4875,
+    'NE': 0.7000,
+    'NYG': 0.5750,
+    'JAC': 0.3750,
+    'BUF': 0.3250,
+    'DAL': 0.4750,
+    'TB': 0.4000,
+    'OAK': 0.3125,
+    'DET': 0.1875,
+    'KC': 0.3625,
+    'STL': 0.2625,
+    'DEN': 0.4750,
+    'BAL': 0.4125,
+    'MIN': 0.3625,
+    'CIN': 0.3750,
+    'HOU': 0.3250,
+    'SF': 0.3125,
+    'SEA': 0.3750,
+    'ATL': 0.4684,
+    'NYJ': 0.4250,
+    'ARI': 0.2625,
+    'PIT': 0.5875,
+    'SD': 0.5125,
+    'WAS': 0.3625,
+    'NO': 0.5625,
+    'CLE': 0.3125,
+    'CHI': 0.4125,
+    'MIA': 0.4000
+}
--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,6 +1,13 @@
 from context import predict
 from context import generate
 
+import pytest
+
+slow = pytest.mark.skipif(
+    not pytest.config.getoption('--runslow'),
+    reason='need --runslow option to run'
+)
+
 def test_open_data():
     data = predict.open_data()
     assert data
@@ -8,10 +15,28 @@ def test_open_data():
 def test_compare_teams():
     game = generate.gen_game(2015, 7, 'CAR', 'Regular')
     results = predict.compare_teams(game)
-    assert results[0]['distance'] == 0.7259199999999999
+    assert results[0]['distance'] == 0.13239
 
 def test_derive_score():
     game = generate.gen_game(2015, 7, 'CAR', 'Regular')
     results = predict.compare_teams(game)
     score = predict.derive_scores(results[0])
-    assert score[0] == 9.64294
+    assert score[0] == 11.15735
+
+def test_predict_winner():
+    game = generate.gen_game(2015, 10, 'NE', 'Regular')
+    results = predict.compare_teams(game)
+    winner = predict.predict_winner(results, game)
+    assert winner == 'NE'
+
+@slow
+def test_predict_winners():
+    correct = predict.predict_winners(2015, 'Regular')
+    assert correct == 186
+
+def test_normalize():
+    game = generate.gen_game(2015, 10, 'NE', 'Regular')
+    game_profile = generate.gen_profile(game)
+    data = predict.open_data()
+    data, game_profile = predict.normalize(data, game_profile)
+    assert data[0]['away_stats'][2] == 0.3012275731822474
